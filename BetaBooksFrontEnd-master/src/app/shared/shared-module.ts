957c4b845a5228375acb776b3b3d40b998e31179
GaragliaArianna <arianna.garaglia@gmail.com>
2026-04-08 15:13:05 +0200

    rimosso inner project

--- BetaBooksFrontEnd-master/src/app/shared/shared-module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { Navbar } from './components/navbar/navbar';
-import { Footer } from './components/footer/footer';
-
-@NgModule({
-  imports: [Navbar, Footer],
-  exports: [Navbar, Footer]
-})
-export class SharedModule {}
